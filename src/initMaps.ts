7d994fe917bf37c6dcff3060267f0d5398856ae4
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-04-28 10:57:18 +0200

    Add map rates and change intial rating

--- src/initMaps.ts
+++ src/initMaps.ts
@@ -5,10 +5,10 @@ import logging from "./config/logging";
 
 export function addNewMaps(count: number) {
     let validMaps = fs
-        .readFileSync("./maps.txt")
+        .readFileSync("./maps.tsv")
         .toString()
         .split("\r\n")
-        .map((n) => parseInt(n));
+        .map((row) => row.split("\t"));
 
     // Shuffle
     for (let i = validMaps.length - 1; i > 0; i--) {
@@ -17,11 +17,21 @@ export function addNewMaps(count: number) {
     }
 
     let mapsToAdd = validMaps.slice(0, count);
+    console.log(mapsToAdd);
 
     Database.connect();
 
-    for (const mapId of mapsToAdd) {
-        EntityModel.createNewMap(mapId)
+    for (const map of mapsToAdd) {
+        let [mapId, diff08X, diff09X, diff10X, diff11X, diff12X] = map;
+        let difficulty = [
+            { rate: 0.8, diff: parseFloat(diff08X) },
+            { rate: 0.9, diff: parseFloat(diff09X) },
+            { rate: 1.0, diff: parseFloat(diff10X) },
+            { rate: 1.1, diff: parseFloat(diff11X) },
+            { rate: 1.2, diff: parseFloat(diff12X) },
+        ];
+
+        EntityModel.createNewMap(parseInt(mapId), difficulty)
             .then((map) => logging.info(`Added ${mapId}`))
             .catch((err) => logging.info(`Skipped ${mapId} (${err})`));
     }
--- src/models/entity.ts
+++ src/models/entity.ts
@@ -1,9 +1,7 @@
-import { getModelForClass, modelOptions, mongoose, prop, Severity } from "@typegoose/typegoose";
+import { DocumentType, getModelForClass, modelOptions, prop, Severity } from "@typegoose/typegoose";
 import config from "../config/config";
 import Requester from "../requester/requester";
-import { EntityDatapointModel, GeneralDatapointModel } from "./datapoint";
-import { DocumentType } from "@typegoose/typegoose";
-import Glicko from "../glicko/glicko";
+import { EntityDatapointModel } from "./datapoint";
 
 type EntityType = "map" | "user";
 type EntityDoc = DocumentType<Entity>;
@@ -15,6 +13,7 @@ type EntityDoc = DocumentType<Entity>;
 class Entity {
     @prop() public entityType!: EntityType;
     @prop() public quaverId!: number;
+    @prop() public mapRate?: number;
 
     public static async createNewUser(quaverId: number): Promise<EntityDoc> {
         let quaverUser = await Entity.fetchQuaverUser(quaverId, 0);
@@ -24,40 +23,27 @@ class Entity {
         return newUser;
     }
 
-    public static async createNewMap(quaverId: number): Promise<EntityDoc> {
-        const result = await EntityModel.findOne({ entityType: "map", quaverId }).exec();
-        if (result) throw "Map already exists";
-        else {
-            let quaverMap = await Entity.fetchQuaverMap(quaverId);
-            if (!quaverMap) throw "Quaver map does not exist";
-            let newMap = await EntityModel.create({ quaverId, entityType: "map" });
+    public static async createNewMap(quaverId: number, difficulty: { rate: number; diff: number }[]): Promise<EntityDoc> {
+        for (const { rate, diff } of difficulty) {
+            const result = await EntityModel.findOne({ entityType: "map", quaverId, mapRate: rate }).exec();
+            if (result) continue;
+            else {
+                let newMap = await EntityModel.create({ quaverId, entityType: "map", mapRate: rate });
 
-            // Apply rating scaling roughly depending on difficulty
-            // Approximation using 4 segmented linear functions
-            // https://docs.google.com/spreadsheets/d/1lfBCM6EAdJ1n-xf8HqR7PEEJDFoqApI8uy5s8ESf45g/edit#gid=247636023
-            //
-            // { qr: 2.5, percentile: 1.0 },
-            // { qr: 7, percentile: 0.55 },
-            // { qr: 18.5, percentile: 0.17 },
-            // { qr: 35, percentile: 0.01 },
-            // { qr: 40, percentile: 0.001 },
+                // Apply rating scaling roughly depending on difficulty
+                // The spread of all difficulty values is linear
+                // https://docs.google.com/spreadsheets/d/1lfBCM6EAdJ1n-xf8HqR7PEEJDFoqApI8uy5s8ESf45g/edit#gid=247636023
 
-            const diff = quaverMap.difficulty_rating;
-            let percentile = 0;
-            const linear = (x1: number, x2: number, y1: number, y2: number) => ((diff - x1) / (x2 - x1)) * (y2 - y1) + y1;
+                const linear = (x1: number, x2: number, y1: number, y2: number) => ((diff - x1) / (x2 - x1)) * (y2 - y1) + y1;
 
-            if (diff >= 40) percentile = 0.001;
-            else if (diff >= 35) percentile = linear(35, 40, 0.01, 0.001);
-            else if (diff >= 18.5) percentile = linear(18.5, 35, 0.17, 0.01);
-            else if (diff >= 7) percentile = linear(7, 18.5, 0.55, 0.17);
-            else if (diff >= 3) percentile = linear(2, 7, 0.999, 0.55);
-            else percentile = 0.999;
+                let ratingAdjustment: number;
+                if (diff >= 40) ratingAdjustment = 1000;
+                else if (diff >= 0) ratingAdjustment = linear(0, 40, -1000, 1000);
+                else ratingAdjustment = -1000;
 
-            // function errs if percentile is exactly 1 or 0
-            let newRating = Glicko.gxeToGlicko(1 - percentile, 200);
-
-            await EntityDatapointModel.createFreshDatapoint(newMap, newRating, 200);
-            return newMap;
+                await EntityDatapointModel.createFreshDatapoint(newMap, 1500 + ratingAdjustment, 200);
+                return newMap;
+            }
         }
     }
 
