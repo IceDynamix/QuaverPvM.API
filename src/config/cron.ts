81360d275ced44e2b17776fbcac823be330dcc94
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-05-15 21:34:05 +0200

    Adjust cron a final time...

--- src/config/cron.ts
+++ src/config/cron.ts
@@ -6,30 +6,30 @@ import { Entity } from "../models/entity";
 
 // seconds? minutes hours date month weekday
 
-let glickoJob = new CronJob("0 0 0 * * *", () => {
-    logging.info("Running glickoJob cron");
-    Glicko.updateAllEmpty()
-        .then(() => logging.info("Finished running glickoJob cron"))
-        .catch((err) => logging.error("Could not run glickoJob cron", err));
-});
-let newMapJob = new CronJob("0 0 0 * * *", () => {
-    logging.info("Running newMapJob cron");
-    let n = 5;
-    Entity.addNewMaps(n)
-        .then(() => logging.info("Finished running newMapJob cron"))
-        .catch((err) => logging.error("Could not run newMapJob cron", err));
-});
-let generalDpJob = new CronJob("0 */2 * * *", () => {
-    logging.info("Running generalDpJob cron");
-    GeneralDatapointModel.createNewDatapoint()
-        .then((dp) => logging.info("Finished running generalDpJob cron", dp))
-        .catch((err) => logging.error("Could not run generalDpJob cron", err));
-});
+const crons = [
+    new CronJob("0 0 * * *", () => {
+        logging.info("Running glickoJob cron");
+        Glicko.updateAllEmpty()
+            .then(() => logging.info("Finished running glickoJob cron"))
+            .catch((err) => logging.error("Could not run glickoJob cron", err));
+    }),
+    new CronJob("0 0 * * *", () => {
+        logging.info("Running newMapJob cron");
+        let n = 5;
+        Entity.addNewMaps(n)
+            .then(() => logging.info("Finished running newMapJob cron"))
+            .catch((err) => logging.error("Could not run newMapJob cron", err));
+    }),
+    new CronJob("*/30 * * * *", () => {
+        logging.info("Running generalDpJob cron");
+        GeneralDatapointModel.createNewDatapoint()
+            .then((dp) => logging.info("Finished running generalDpJob cron", dp))
+            .catch((err) => logging.error("Could not run generalDpJob cron", err));
+    }),
+];
 
 function startCrons() {
-    glickoJob.start();
-    newMapJob.start();
-    generalDpJob.start();
+    crons.forEach((c) => c.start());
     logging.info("Instantiated all cron jobs");
 }
 
