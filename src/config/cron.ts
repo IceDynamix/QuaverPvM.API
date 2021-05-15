480d881f1541426f8a6ed96571816b6ea257dc0b
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-05-15 19:53:27 +0200

    Log cron errors

--- src/config/cron.ts
+++ src/config/cron.ts
@@ -7,14 +7,20 @@ import { Entity } from "../models/entity";
 // seconds? minutes hours date month weekday
 
 let glickoJob = new CronJob("00 00 00 * * *", () => {
-    Glicko.updateAllEmpty().then(() => logging.info("Ran glickoJob cron"));
+    Glicko.updateAllEmpty()
+        .then(() => logging.info("Ran glickoJob cron"))
+        .catch((err) => logging.error("Could not run glickoJob cron", err));
 });
 let newMapJob = new CronJob("00 00 00 * * *", () => {
     let n = 5;
-    Entity.addNewMaps(n).then(() => logging.info("Ran newMapJob cron"));
+    Entity.addNewMaps(n)
+        .then(() => logging.info("Ran newMapJob cron"))
+        .catch((err) => logging.error("Could not run newMapJob cron", err));
 });
 let generalDpJob = new CronJob("00 */30 * * * *", () => {
-    GeneralDatapointModel.createNewDatapoint().then((dp) => logging.info("Ran generalDpJob cron", dp));
+    GeneralDatapointModel.createNewDatapoint()
+        .then((dp) => logging.info("Ran generalDpJob cron", dp))
+        .catch((err) => logging.error("Could not run generalDpJob cron", err));
 });
 
 function startCrons() {
