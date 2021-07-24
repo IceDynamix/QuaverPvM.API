b34f3ff3cc96137f1d788834992bc3a819a9f96b
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-07-24 18:42:30 +0200

    Fix /user route

--- server.ts
+++ server.ts
@@ -126,7 +126,7 @@ class Server {
 
     static setupRoutes() {
         app.get("/me", UserController.selfGET);
-        app.get("/user", UserController.selfGET);
+        app.get("/user", UserController.GET);
 
         app.get("/logout", (req: Request, res: Response) => {
             req.logout();
