7d2b6cccde820dc539f02854b1b96d2444373b4c
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-07-27 14:13:41 +0200

    Add match schema and ongoing match

---
+++ prisma/migrations/20210727133426_match/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `matches` on the `map` table. All the data in the column will be lost.
+  - You are about to drop the column `matches` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `map` DROP COLUMN `matches`,
+    ADD COLUMN `matchesPlayed` INTEGER NOT NULL DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE `user` DROP COLUMN `matches`,
+    ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `matchesPlayed` INTEGER NOT NULL DEFAULT 0;
+
+-- CreateTable
+CREATE TABLE `Match` (
+    `matchId` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `result` ENUM('ONGOING', 'WIN', 'RESIGN', 'TIMEOUT') NOT NULL DEFAULT 'ONGOING',
+    `userId` INTEGER NOT NULL,
+    `mapId` INTEGER NOT NULL,
+    `mapRate` DOUBLE NOT NULL,
+
+    INDEX `Match.userId_index`(`userId`),
+    INDEX `Match.mapId_mapRate_index`(`mapId`, `mapRate`),
+    PRIMARY KEY (`matchId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Match` ADD FOREIGN KEY (`userId`) REFERENCES `User`(`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Match` ADD FOREIGN KEY (`mapId`, `mapRate`) REFERENCES `Map`(`mapId`, `mapRate`) ON DELETE CASCADE ON UPDATE CASCADE;
