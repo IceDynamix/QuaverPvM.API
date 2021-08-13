212ddde40f0e3dbf2a78210d802ec22447a95aa0
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-08-13 23:01:40 +0200

    Fix integer division issue when creating mappool

--- prisma/createMapDataset.ts
+++ prisma/createMapDataset.ts
@@ -2,6 +2,9 @@ import fs from "fs";
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 import yaml from "js-yaml";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 async function main() {
     if (!process.env.QUAVER_DB_PATH) throw "QUAVER_DB_PATH env not set";
@@ -28,19 +31,30 @@ async function main() {
         FROM Map
         WHERE RankedStatus = 2 -- Ranked
             AND Mode = 1 -- 4k
-            AND LongNoteCount/(RegularNoteCount + LongNoteCount) < 0.6
-            AND SongLength > 120000
-            AND SongLength < 300000
+            AND cast(LongNoteCount as real)/(RegularNoteCount + LongNoteCount) < 0.6 -- <60% LN
+            AND SongLength > 120000 -- 2 minutes
+            AND SongLength < 300000 -- 5 minutes
     `);
     const count = maps.length;
     console.log(`Found ${count} maps`);
 
     const nsvMaps = maps.filter((map, i) => {
         const path = `${process.env.QUAVER_SONGS_PATH}/${map.Directory}/${map.MapId}.qua`;
-        if (!fs.existsSync(path)) return false;
-        console.log(`${i}/${count}\tScanning ${path}`);
+        if (!fs.existsSync(path)) {
+            console.log(`${i}/${count}\tRejected ${path} (File does not exist)`);
+            return false;
+        }
+
         const mapContent: any = yaml.load(fs.readFileSync(path, "utf8"));
-        return (mapContent.BPMDoesNotAffectScrollVelocity || mapContent.TimingPoints.length == 1) && mapContent.SliderVelocities.length == 0;
+        const timingPointsAreNsv = mapContent.BPMDoesNotAffectScrollVelocity || mapContent.TimingPoints.length === 1;
+        const nsv = mapContent.SliderVelocities.length === 0;
+
+        if (!timingPointsAreNsv && nsv) {
+            console.log(`${i}/${count}\tRejected ${path} (File was not NSV)`);
+            return false;
+        }
+
+        return true;
     });
 
     console.log(`Found ${nsvMaps.length} NSV maps`);
