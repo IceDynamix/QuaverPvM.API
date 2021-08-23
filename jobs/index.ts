ca09d3b702253caeb5f5445d8388cb2ae9b8cc65
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-08-23 11:28:02 +0200

    Add updateAllUserRd to job scheduling

---
+++ jobs/index.ts
@@ -0,0 +1,9 @@
+import Ranking from "../src/ranking";
+
+export default [
+    {
+        name: "updateAllUserRd",
+        interval: "at 00:00",
+        path: Ranking.updateAllUserRd,
+    },
+];
--- server.ts
+++ server.ts
@@ -1,11 +1,17 @@
 import Server from "./src/config/express";
 import Ranking from "./src/ranking";
 import Matching from "./src/matching";
+import Bree from "bree";
+import jobs from "./jobs";
 
 async function main() {
     await Matching.cleanUpAllMatches();
     await Ranking.seedUserLeaderboard();
     await Ranking.seedMapLeaderboard();
+
+    const bree = new Bree({ jobs });
+    bree.start();
+
     new Server();
 }
 
