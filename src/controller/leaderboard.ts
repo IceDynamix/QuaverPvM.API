ed0e44d49425eb9d11d89a63906dd909652bacd0
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-12-18 22:41:44 +0100

    Add full leaderboard

--- src/controller/leaderboard.ts
+++ src/controller/leaderboard.ts
@@ -6,11 +6,16 @@ const pageSize = 50;
 
 export default class LeaderboardController {
     public static async GET(req: Request, res: Response, next: Function) {
-        const { page } = req.query;
+        const { page, full } = req.query;
 
         const pageNumber = page ? Math.max(parseInt(page.toString()), 0) : 0;
+
+        let filter;
+        if (full) filter = { matchesPlayed: { gte: 10 }, banned: false };
+        else filter = { rd: { lte: Ranking.rankedRdThreshold }, banned: false };
+
         let results = await prisma.user.findMany({
-            where: { rd: { lte: Ranking.rankedRdThreshold }, banned: false },
+            where: filter,
             orderBy: { rating: "desc" },
             skip: pageSize * pageNumber,
             take: pageSize,
