1916927b4c66ef5f22d6280e673df29ef2d63bc0
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2022-02-02 02:04:33 +0100

    Fix incorrect ranked status condition

--- src/controller/stats.ts
+++ src/controller/stats.ts
@@ -5,12 +5,21 @@ import Ranking from "../ranking";
 export default class StatsController {
     public static async GET(req: Request, res: Response, next: Function) {
         const userCount = await prisma.user.count({ where: { banned: false } });
-        const rankedUserCount = await prisma.user.count({ where: { banned: false, rd: { lte: Ranking.rankedRdThreshold } } });
-        const mapCount = await prisma.map.count();
-        const rankedMapCount = await prisma.map.count({ where: { rd: { lte: Ranking.rankedRdThreshold } } });
-        const matchCount = await prisma.match.count();
+        const rankedUserCount = await prisma.user.count({
+            where: {
+                banned: false,
+                matchesPlayed: { gte: Ranking.rankedMatchesPlayedUserThreshold },
+            },
+        });
 
-        // TODO: Rank Thresholds
+        const mapCount = await prisma.map.count();
+        const rankedMapCount = await prisma.map.count({
+            where: {
+                matchesPlayed: { gte: Ranking.rankedMatchesPlayedMapThreshold },
+            },
+        });
+
+        const matchCount = await prisma.match.count();
 
         res.json({ userCount, rankedUserCount, mapCount, rankedMapCount, matchCount });
     }
